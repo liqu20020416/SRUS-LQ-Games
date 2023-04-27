d783e8d9faac53631763a2e4f47e598f9c3a2f96
Li Qu <20002116@tafe.wa.edu.au>
2023-04-27 14:30:57 +0800

    v1.3

--- app/player.py
+++ app/player.py
@@ -5,6 +5,7 @@ class Player:
         self._name = name
         self.hashed_password = None
         self.argon2Hasher = PasswordHasher()
+        self.score = 0
 
     def __str__(self):
         return f"Player name is : {self._name} \n Player ID is: {self._uid}"
@@ -25,4 +26,31 @@ class Player:
             return True
         else:
             return False
+
+    @property
+    def get_score(self):
+        return self.score
+
+    @get_score.setter
+    def set_score(self, value):
+        self.score = value
+
+    def __eq__(self, compareWith):
+        return self.score == compareWith.score
+
+    def __lt__(self, compareWith):
+        return self.score < compareWith.score
+
+    def __gt__(self, compareWith):
+        return self.score > compareWith.score
+
+    @staticmethod
+    def sort_score(players):
+        for i in range(len(players)):
+            min_score = i
+            for j in range(i + 1, len(players)):
+                if players[min_score] < players[j]:
+                    min_score = j
+            players[i], players[min_score] = players[min_score], players[i]
+
 
--- test/player_test.py
+++ test/player_test.py
@@ -6,7 +6,12 @@ from app.player import Player
 class TestCase(unittest.TestCase):
     def setUp(self):
         self.test_case = Player("00000", "Li")
-
+        self.test_case.score = 125
+        self.test_case1 = Player("00001", "Li1")
+        self.test_case1.score = 130
+        self.test_case2 = Player("00002", "Li2")
+        self.test_case2.score = 150
+        self.test_score_list = [self.test_case, self.test_case1, self.test_case2]
     def test_uid(self):
         self.assertEqual(self.test_case.uid, "00000")
 
@@ -20,4 +25,17 @@ class TestCase(unittest.TestCase):
     def test_verify_password(self):
         self.test_case.add_password("20020416")
         self.assertTrue(self.test_case.argon2Hasher.verify(self.test_case.hashed_password, "20020416"))
+
+    def test_eq(self):
+        self.assertFalse(self.test_case == self.test_case1)
+
+    def test_lt(self):
+        self.assertTrue(self.test_case < self.test_case2)
+
+    def test_gt(self):
+        self.assertTrue(self.test_case2 > self.test_case)
+
+    def test_sort_score(self):
+        Player.sort_score(self.test_score_list)
+        self.assertEqual(self.test_score_list, [self.test_case2, self.test_case1, self.test_case])
 
