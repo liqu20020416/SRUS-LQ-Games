3ad44c6bba1a12e5cc5f86b9c0b517acdbf340e4
Li Qu <20002116@tafe.wa.edu.au>
2023-05-04 15:01:06 +0800

    v4.6

--- app/player_bst.py
+++ app/player_bst.py
@@ -16,24 +16,43 @@ class PlayerBST:
 
     def insert_player(self, player: PlayerBNode, name_key=None):
         if name_key is None:
-            name_key = self.root
+            name_key = self._root
             if name_key is None:
-                self.root = player
+                self._root = player
                 return
 
-        if name_key > player:
+        if str(name_key) > str(player):
             if name_key.right_tree is None:
                 name_key.right_tree = player
                 return
             self.insert_player(player, name_key.right_tree)
 
-        elif name_key < player:
+        elif str(name_key) < str(player):
             if name_key.left_tree is None:
                 name_key.left_tree = player
                 return
             self.insert_player(player, name_key.left_tree)
 
         else:
-            self.insert_player(player, name_key.right_tree)
+            self.insert_player(player)
             player.name = str(input("Duplicated Name! Please enter player name:"))
 
+    def search(self, name: str, name_key=None):
+        if name_key is None:
+            name_key = self._root
+            if name_key is None:
+                return print("This BST is empty!")
+
+        if str(name_key.player.name) > str(name):
+            if name_key.right_tree is None:
+                return print("The name is not exist!")
+            self.insert_player(name, name_key.right_tree)
+
+        elif str(name_key.player.name) < str(name):
+            if name_key.left_tree is None:
+                return print("The name is not exist!")
+            self.insert_player(name, name_key.left_tree)
+
+        else:
+            self.insert_player(name_key.player.name)
+            name_key.player.name = str(input("Duplicated Name! Please enter player name:"))
--- test/player_bst_test.py
+++ test/player_bst_test.py
@@ -13,5 +13,14 @@ class TestPlayerBST(unittest.TestCase):
         self.bst_list = PlayerBST()
 
     def test_insert_player(self):
-        self.bst_list.insert_player(self.player_case)
+        self.bst_list.insert(self.player_case)
         self.assertEqual(self.bst_list.root, self.player_case)
+
+    def test_insert_player_right(self):
+        self.bst_list.insert_player(self.player_case)
+        self.bst_list.insert_player(self.player_case1)
+        self.assertEqual(self.bst_list.root.right_tree, self.player_case1)
+
+    def test_search(self):
+        self.bst_list.search(self.player_case)
+        self.assertEqual(self.bst_list.root, None)
